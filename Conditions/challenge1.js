a1cf090e7c3e4ec7f1f7fdf4139c6e356334f562
ikramhasnane <ikramhasnane26@gmail.com>
2026-09-17 16:32:21 +0100

    le nombre afficher pair ou impair

--- Conditions/challenge1.js
+++ Conditions/challenge1.js
@@ -1,5 +1,5 @@
 const prompt=require("prompt-sync")();
-let nombre=parseInt("Entrez un nombre entier:");
+let nombre=parseInt(prompt("Entrer un nombre entier:"));
 if ( nombre % 2 === 0) {
     console.log("nombre pair");
 } else {
