bcfbefd0b9d79993cc1698668325c12deff4fe08
ikramhasnane <ikramhasnane26@gmail.com>
2026-09-17 14:27:55 +0100

    transforme la distance en kilometre à yards

---
+++ challenge 3.js
@@ -0,0 +1,5 @@
+const prompt= require ("prompt-sync")();
+let distanceKm = Number(prompt("Entez la distance en kilométre:"));
+let distanceYards = distanceKm*1093.61
+console.log (`la distance en yards est: ${distanceYards}`);
+
