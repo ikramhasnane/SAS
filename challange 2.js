45e5b9fd42d415263c11e7ebc652aa36e42878e9
ikramhasnane <ikramhasnane26@gmail.com>
2026-09-17 12:44:23 +0100

    solve chalange 2

---
+++ challange 2.js
@@ -0,0 +1,4 @@
+const prompt=require("prompt-sync")();
+let tempCelsius = Number ( prompt("Entrez la température en celsius"))
+let tempKelvin = tempCelsius + 273.15;
+console.log (`${tempCelsius}°C correspond à ${tempKelvin}K`);
