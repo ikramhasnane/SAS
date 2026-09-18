d975c0955133c1172931e79337e35abbd82b6253
ikramhasnane <ikramhasnane26@gmail.com>
2026-09-18 12:24:49 +0100

    calcule la factorielle d'un nombre entier positif n entrer par utilisateur

---
+++ Les boucles/challenge2.js
@@ -0,0 +1,13 @@
+const prompt=require ("prompt-sync")();
+
+let n =parseInt(prompt("Entrez un entier positif pour la factorielle:"));
+
+let factorielle=1;
+
+
+for (let i = 1; i <= n; i++)
+{
+
+    factorielle = i*factorielle
+}
+console.log(`${n}=${factorielle}`);
