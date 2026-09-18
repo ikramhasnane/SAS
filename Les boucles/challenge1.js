85e1473e36a472c1ae129af11a7e0df0a6066768
ikramhasnane <ikramhasnane26@gmail.com>
2026-09-18 10:47:19 +0100

    répéter 10 fois la meme opération

---
+++ Les boucles/challenge1.js
@@ -0,0 +1,8 @@
+const prompt=require("prompt-sync")();
+let n=parseInt(prompt("Entrez le nombre pour la table de multiplication:"));
+console.log(`Table de multiplication de ${n}:`);
+ ;for (let i = 1; i <= 10; i++)  {
+
+
+console.log(`${n}* ${i} = ${resultat}`); 
+}
