65b8134ea8040ce6eac4dda03473d83080bdd0cd
ikramhasnane <ikramhasnane26@gmail.com>
2026-09-19 14:07:04 +0100

    afficher la somme total

---
+++ Tableaux/challenge3.js
@@ -0,0 +1,13 @@
+const prompt= require("prompt-sync")();
+
+
+let nombre=parseInt(prompt("combien le nombre élément afficher "));
+
+const tableaux=[];
+let valeur = 0;
+for(let i=0;i<nombre;i++)
+    {
+    tableaux[i]=(parseInt("entrer les élément du tableau"));
+    valeur =   valeur + tableaux[i];
+    
+}
