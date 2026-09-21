df1d44fbbd1a75f6e738584dc8f63ddebd1af37b
ikramhasnane <ikramhasnane26@gmail.com>
2026-09-21 16:08:14 +0000

    inversion du chaine

---
+++ Chaines/challenge5.js
@@ -0,0 +1,8 @@
+const prompt=require("prompt-sync")();
+const n=prompt("Entrer une chaine de caractére");
+let inverse="";
+for(let i=n.length-1;i>=0; i--){
+inverse=inverse+n[i];
+    
+}
+console.log(inverse)
