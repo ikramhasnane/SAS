e47ed04e78ffa1c1f22451b6b0c047e9d34aa737
ikramhasnane <ikramhasnane26@gmail.com>
2026-09-22 16:50:48 +0000

    afficher

---
+++ Function/challenge1.js
@@ -0,0 +1,5 @@
+function additionner(a,b)
+   {return a+b;
+}
+let resultat=additionner(1,2);
+console.log(resultat);
---
+++ Function/challenge2.js
@@ -0,0 +1,5 @@
+function multiplier(a,b){
+    return a*b;
+} 
+let score=multiplier(1,2);
+console.log(score)
