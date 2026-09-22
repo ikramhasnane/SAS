006f06d95a865922af5b70823586fdcb45ad4cd4
ikramhasnane <ikramhasnane26@gmail.com>
2026-09-22 15:34:48 +0000

    afficher

---
+++ Objet/challenge5.js
@@ -0,0 +1,12 @@
+function livre (titre,auteur,année){
+    return{
+    titre: "boite",
+    auteur:"EFRIOUI",
+    année:1998,
+    }
+};
+
+     const livred = livre()
+console.log(livred.titre);
+console.log(livred.auteur);
+console.log(livred.année);
