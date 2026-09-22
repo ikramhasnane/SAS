2b731ed8046761c61120b2f0d781c87dc1c61d67
ikramhasnane <ikramhasnane26@gmail.com>
2026-09-22 12:51:04 +0000

    afficher

--- Objet/challenge3.js
+++ Objet/challenge3.js
@@ -1,4 +1,9 @@
 const rectangle={
     longueur: 4 ,
     largeur:2 ,
-}
+};
+function calculAir(rectangle){
+    return rectangle.longueur*rectangle.largeur
+} 
+const air=calculAir(rectangle);
+ console.log(air);
